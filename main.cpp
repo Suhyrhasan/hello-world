05a32fa5dab2a268206227ff14c97d0f287bbb41
Suhyrhasan <51502072+Suhyrhasan@users.noreply.github.com>
2020-01-15 16:56:46 -0800

    Update and rename README.md to main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+ * AUTHOR	   : Suhyr Hasan and Tanner Cordero
+ * STUDENT ID  : 1022520 and
+ * Lab # 2     : Test using GITHUB
+ * CLASS 	   : CS1D
+ * SECTION 	   : MW 2:30p - 4:50p
+ * DUE DATE	   : 01/15/2020
+ *****************************************************************************/
+#include <iostream>
+using namespace std;
+int hello1Function(string name1);
+int hello2Function(string name2);
+/******************************************************************************
+ * Test using GITHUB
+ * ----------------------------------------------------------------------------
+ * Hello World in C++
+ * ----------------------------------------------------------------------------
+ *****************************************************************************/
+int main()
+{
+	string name1="Suhyr Hasan";
+	hello1Function(name1);
+	string name2="Tanner Cordero";
+	hello2Function(name2);
+	return 0;
+}
