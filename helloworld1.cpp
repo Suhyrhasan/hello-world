55c313390997a7fb1cbefc98dafdead266ccbc69
Suhyrhasan <51502072+Suhyrhasan@users.noreply.github.com>
2020-01-15 16:55:28 -0800

    Create helloworld1.cpp

---
+++ helloworld1.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * Function hello1Function
+ * ____________________________________________________________________________
+ * This function reads in a name1 and then outputs a string to the console.
+ * ____________________________________________________________________________
+ * PRE-CONDITIONS
+ * 	  The following parameters need to have a defined value prior to calling
+ * 	  the function:
+ * 	                name1 : 1st name
+ *
+ * POST-CONDITIONS
+ * 	  This function outputs a string.
+ ******************************************************************************/
+int hello1Function(string name1)
+{
+	cout << "hello World " << name1 << endl;
+	return 0;
+}
